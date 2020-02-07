3631b04b69041c72cb118d4a8027f7e45563e1ec
Liang HE <lhe@tripadvisor.com>
2020-02-07 17:08:47 +1100

    Fragments

---
+++ app/src/main/java/life/ppgoal/iching_learnig_tool/fragments/DoubleHexagramDetailFrag.java
@@ -0,0 +1,32 @@
+package life.ppgoal.iching_learnig_tool.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import life.ppgoal.iching_learnig_tool.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DoubleHexagramDetailFrag extends Fragment {
+
+
+    public DoubleHexagramDetailFrag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_double_hexagram_detail, container, false);
+    }
+
+}
---
+++ app/src/main/java/life/ppgoal/iching_learnig_tool/fragments/DoubleHexagramListFrag.java
@@ -0,0 +1,27 @@
+package life.ppgoal.iching_learnig_tool.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import life.ppgoal.iching_learnig_tool.R;
+
+public class DoubleHexagramListFrag extends Fragment {
+
+
+
+    public DoubleHexagramListFrag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_double_hexagram_list, container, false);
+    }
+
+}
